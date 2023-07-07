cfdbde524ac6446a2f98912c5e44fbf25d0e1b08
Justice Isreal <justiceyba@gmail.com>
2023-07-07 02:01:15 +0100

    server first push

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-
+// jj
 // routes
 const userAccess = require("./Route/UserRoute");
 const homeItems = require("./Route/HomepageRoute");
